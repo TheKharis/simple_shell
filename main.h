59825c71eb5138ff66179185434aa8b320174679
JoshAmpofo <joshampofo92@gmail.com>
2022-12-17 16:15:25 +0000

    Add more prototypes

--- main.h
+++ main.h
@@ -8,6 +8,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#define BUFFER_SIZE 1024
+#define TOKEN_DELIMITERS " \n"
 
+/* execute PATH */
+char *search_path(const char *command);
 
 #endif /* _MAIN_H */
