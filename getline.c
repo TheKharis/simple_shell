c24c3251171857a8e1c168031085de4b20fb0852
JoshAmpofo <joshampofo92@gmail.com>
2022-12-20 06:58:07 +0000

    String input function

---
+++ getline.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _getline - take user input from command line
+ *
+ * Return: void
+ */
+void _getline(char **line)
+{
+	size_t n; 
+	ssize_t char_read;
+
+	char_read = getline(line, &n, stdin);
+	if (char_read == -1)
+	{
+		printf("\n");
+		free(line);
+		exit(1);
+	}
+}
