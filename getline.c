459166d7617105876b396152ba194dd86703adb2
JoshAmpofo <joshampofo92@gmail.com>
2022-12-21 09:21:51 +0000

    Minor refactor

--- getline.c
+++ getline.c
@@ -2,12 +2,12 @@
 
 /**
  * _getline - take user input from command line
- *
+ * @line: input string from command line
  * Return: void
  */
 void _getline(char **line)
 {
-	size_t n; 
+	size_t n;
 	ssize_t char_read;
 
 	char_read = getline(line, &n, stdin);
