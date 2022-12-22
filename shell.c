a8d051455378d01718144eb7cb9efc2d472700bf
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-12-22 22:43:11 +0000

    Adding more functions

--- shell.c
+++ shell.c
@@ -1,50 +1,134 @@
 #include "main.h"
+
+void sig_handler(int sig);
+int execute(char **args, char **front);
+
 /**
- * main - execute a simple shell
- * @ac: argument count
- * @av: argument vector
- * Return: Always 0.
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
  */
-int main(__attribute__((unused))int ac, char **av)
+void sig_handler(int sig)
 {
-	char *lineptr = NULL;
-	char *token = NULL, *token_args[30];
-	pid_t pid;
-	size_t n = 0;
-	int i = 0;
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
+}
+
+/**
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
+ *
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
+ */
+int execute(char **args, char **front)
+{
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
+
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
+
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+	return (ret);
+}
+
+/**
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: The return value of the last executed command.
+ */
+int main(int argc, char *argv[])
+{
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
+
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
+
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
 
 	while (1)
 	{
-		printf("$ "); /* prompt */
-		if (getline(&lineptr, &n, stdin) < 0)
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
 		{
-			printf("\n");
-			free(lineptr);
-			exit(1);
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
 		}
-		pid = fork(); /* create fork */
-		if (pid < 0)
-		{
-			perror("fork");
-			exit(1);
-		} else if (pid == 0)
-		{
-			token = strtok(lineptr, " \n"); /* split strings into words */
-			while (token != NULL)
-			{
-				token_args[i] = token; /* token arguments */
-				i++;
-				token = strtok(NULL, " \n"); /* second string */
-			} token_args[i] = NULL; /* add null character to end of argument */
-			if (execve(token_args[0], token_args, NULL) < 0) /* execute commands */
-			{
-				perror(av[0]);
-				exit(1);
-			}
-		}
-		else
-			wait(NULL);
 	}
-	free(lineptr);
-	return (0);
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
